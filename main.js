13c2b783395bf1e90986eefd67dd9ecc22e8f0fe
Klaudia <oska456@wp.pl>
2020-08-06 13:38:19 +0200

    add function addNewTask

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ let $todoInput; //miejsce, gdzie uzytkownik wpisuje treść zadania
 let $alertInfo; //informacja o braku zadań/koniecznosci dodania tekstu
 let $addBtn; //prycisk add
 let $ulList; // lista zadań
+let $newTask; // nowo dodany li w funkcji addNewTask
+
 
 const main = () => {
     prepareDOMElements();
@@ -11,32 +13,35 @@ const main = () => {
 
 // pobieranie elementów 
 const prepareDOMElements = () => {
-    $todoInput=document.querySelector('.todoInput');
-    $alertInfo=document.querySelector('.alertInfo');
-    $addBtn=document.querySelector('.addBtn');
-    $ulList=document.querySelector('.todoList ul');
+    $todoInput = document.querySelector('.todoInput');
+    $alertInfo = document.querySelector('.alertInfo');
+    $addBtn = document.querySelector('.addBtn');
+    $ulList = document.querySelector('.todoList ul');
 
 };
 
+const addNewTask = () => {
+    if ($todoInput.value !== '') {
+        $newTask.document.createElement('li');
+        $newTask.innerText='$todoInput.value';
+
+
+    } else {
+        alertInfo.innerText = 'Wpisz treść zadania'
+    };
+};
+
+
+
+
+
+
+
+
 //nasłuchiwanie elemtów
-const prepareDOMEvents = () => {};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+const prepareDOMEvents = () => {
+    addBtn.addEventListener('click', addNewTask);
+};
 
 
 
