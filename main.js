a0ac804832f392e5efbcb247bb91a828ec7208a7
Klaudia <oska456@wp.pl>
2020-08-14 17:50:04 +0200

    giving the rest of the functionality JavaScript

--- main.js
+++ main.js
@@ -5,6 +5,18 @@ let $ulList; // lista zadań
 let $newTask; // nowo dodany li w funkcji addNewTask
 
 
+let $popup; //pobieramy popup
+let $popupInfo; //alert jak się doda pusty tekst
+let $editetTodo; //edytowany todo
+let $popupInput; //tekst wpisywany w inpucie w popupie
+let $addPopupBtn; //przycisk "zatwierdź"
+let $closeTodoBtn; //przycisk zamknięcia popupa
+
+let $idNumber = 0; // dzieki temu do kazdego nowego dodanego todo bedzie dodawany id
+
+let $allTasks;
+
+
 const main = () => {
     prepareDOMElements();
     prepareDOMEvents();
@@ -18,20 +30,33 @@ const prepareDOMElements = () => {
     $addBtn = document.querySelector('.addBtn');
     $ulList = document.querySelector('.todoList ul');
 
+    $popup = document.querySelector('.popup');
+    $popupInfo = document.querySelector('.popupInfo');
+    $popupInput = document.querySelector('.popupInput');
+    $addPopupBtn = document.querySelector('.accept');
+    $closeTodoBtn = document.querySelector('.cancel');
+
+    $allTasks=$ulList.getElementsByTagName('li');
+
 };
 
 //nasłuchiwanie elemtów
 const prepareDOMEvents = () => {
     $addBtn.addEventListener('click', addNewTask);
     $ulList.addEventListener('click', checkClick);
+    $closeTodoBtn.addEventListener('click', closePopup);
+    $addPopupBtn.addEventListener('click', changeTodo);
+    $todoInput.addEventListener('keyup', enterCheck)
 };
 
 
 
 const addNewTask = () => {
     if ($todoInput.value !== '') {
+        $idNumber++; // za każdym razem gdy coś wpiszemy, bedzie dodawane do taska id
         $newTask = document.createElement('li');
         $newTask.innerText = $todoInput.value;
+        $newTask.setAttribute('id', `todo-${$idNumber}`); //do kazdego dodanego taska dodaje sie id który za każdym razem jest inkrementowany 
         $ulList.appendChild($newTask); //utworzenie elementu li ktory bedzie przechowywał treść któr wpisze uzytkownik
 
         $todoInput.value = '';
@@ -42,7 +67,16 @@ const addNewTask = () => {
     };
 };
 
+//czy uzytkownik wcisnął enter
+const enterCheck=()=>{
+    if(event.keyCode===13){
+        addNewTask();
+    }
+};
 
+
+
+// tworzenie przyciskow edycji, usuwania i 'gotowe'
 const createToolsArea = () => {
     const toolsPanel = document.createElement('div');
     toolsPanel.classList.add('tools');
@@ -67,16 +101,55 @@ const createToolsArea = () => {
 };
 
 const checkClick = (e) => {
-    if (e.target.closest('button').classList.contains('complete')) 
-    {
+    if (e.target.closest('button').classList.contains('complete')) {
         e.target.closest('li').classList.toggle('completed');
         e.target.closest('button').classList.toggle('completed');
-    }else if (e.target.closest('button').className === 'edit'){
-        console.log(e.target);
-    }else if(e.target.closest('button').className === 'delete'){
+    } else if (e.target.closest('button').className === 'edit') {
+        editTask(e);
+    } else if (e.target.closest('button').className === 'delete') {
+        deleteTask(e);
+    }
+};
+
+//edycja zadania
+const editTask = (e) => {
+    const oldTodo = e.target.closest('li').id;
+    $editetTodo = document.getElementById(oldTodo);
+    $popupInput.value=$editetTodo.firstChild.textContent;
+
+    $popup.style.display = 'flex';
+
+};
+
+const changeTodo=()=>{
+    if($popupInput.value !==''){
+        $editetTodo.firstChild.textContent=$popupInput.value;
+        $popup.style.display = 'none';
+        $popupInfo.innerText='';
+    }else{
+        $popupInfo.innerText='Musisz podać jakąś treść!';
+    }
+};
+
+
+//zamknięcie popupa
+const closePopup = () => {
+    $popup.style.display = 'none';
+    $popupInfo.innerText='';
+};
+
+//usuwanie zadania
+const deleteTask=(e)=>{
+    const deleteTodo=e.target.closest('li');
+    deleteTodo.remove();
+
+    if($allTasks.length === 0){
+        $alertInfo.innerText='Brak zadań na liście.';
+    }else{
 
     }
 };
+
 
 
 //dodatkowe zabezpieczenie, żeby załadował się najpierw HTML a później JS; uporządkowanie//
